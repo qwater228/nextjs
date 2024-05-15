24e3ef88966e9bdba190768eacea95aa48e82ca4
beja <dubanaevbektur23@gmail.com>
2024-05-15 19:05:33 +0300

    hui

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          <p className="text-3xl bg-white text-black px-20 pt-20 pb-20 rounded-3xl mtx-200 ">fjnini</p>
+      </div>
+    </main>
+  );
+}
